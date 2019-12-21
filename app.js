c88c5dc76efeb573344952f05b707b792da8b09e
Ivo <ivokalendar@icloud.com>
2019-12-21 08:38:31 +0100

    External files

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 
 const express = require('express')
 const app = express()
+let members = require('./apis/members')
 
-let members = [{
-	id: 1,
-	name: 'Member No. 1',
-	email: 'adress1@email.com',
-	status: 'active'
-},{
-	id: 2,
-	name: 'Member No. 2',
-	email: 'adress2@email.com',
-	status: 'active'
-},{
-	id: 3,
-	name: 'Member No. 3',
-	email: 'adress3@email.com',
-	status: 'active'
-}]
+app.use(express.static('public'))
 
 let port = process.env.PORT || 3000
 app.listen(port, console.log(`Started on port: ${port}...`))
 
 app.get('/', (req,res) => {
-	res.send('hello')
+	res.sendFile('index.html')
 })
 
 app.get('/api/members', (req,res) => {
